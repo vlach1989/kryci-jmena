04db5a2408feefa7c73121ac535877aeb4e80e92
vlach1989 <vlach1989@gmail.com>
2020-07-07 18:57:57 +0200

    Updates

---
+++ precache-manifest.3476eacb2a68d59903854de0a1ce147b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3d5156d81ce0abe1e847ecc8d475f2",
+    "url": "/kryci-jmena/index.html"
+  },
+  {
+    "revision": "3bd3af9134e1dc5408d3",
+    "url": "/kryci-jmena/static/css/main.9a5c6f63.chunk.css"
+  },
+  {
+    "revision": "12f1e36800bc4ff114da",
+    "url": "/kryci-jmena/static/js/2.dcf8fc44.chunk.js"
+  },
+  {
+    "revision": "6a9445b8c105ae6f11352a8979f8f003",
+    "url": "/kryci-jmena/static/js/2.dcf8fc44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd3af9134e1dc5408d3",
+    "url": "/kryci-jmena/static/js/main.6bb4ce62.chunk.js"
+  },
+  {
+    "revision": "3356551f9438c6a635df",
+    "url": "/kryci-jmena/static/js/runtime-main.35cdd8ac.js"
+  }
+]);
