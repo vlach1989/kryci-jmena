02d4e614b63ac69175e99efbbcd4368877e8ba86
vlach1989 <vlach1989@gmail.com>
2020-07-18 09:33:37 +0200

    custom words generator

--- src/components/words/WordsGenerator.js
+++ src/components/words/WordsGenerator.js
@@ -9,15 +9,23 @@ const STEP = 24;
 class WordsGenerator extends React.PureComponent {
     constructor(props) {
         super(props);
+        this.state = {
+          text: ""
+        };
+        this.onChange = this.onChange.bind(this);
     }
 
     onGenerate() {
         var element = document.getElementById('words');
-        html2pdf(element);
+        html2pdf(element, {filename: 'slova.pdf'});
+    }
+
+    onChange(e) {
+        this.setState({text: e.target.value})
     }
 
     render() {
-        const allWordsList = _.uniq(words.toUpperCase().replace(/ /g,'').split(","));
+        const allWordsList = _.uniq(this.state.text.toUpperCase().replace(/ /g,'').split(","));
         let listPerPage = [];
 
         for (let i=0; i < allWordsList.length; i+=STEP) {
@@ -27,12 +35,14 @@ class WordsGenerator extends React.PureComponent {
         return (
             <div className="words-generator">
                 <h2>Vytváření slov</h2>
+                <p>Do pole níže zadej slova oddělená čárkou:</p>
+                <textarea rows="8" onChange={this.onChange} value={this.state.text} placeholder="Sem zadej slova oddělená čárkou"/>
                 <button onClick={this.onGenerate}>Ulož do PDF</button>
                 <div className="words" id="words">
-                    {listPerPage.map(page =>
+                    {listPerPage.map((page, index) =>
                         <>
                             <div className="word-page" id="page">
-                                {page.map(word => {
+                                {page.map((word,index) => {
                                     const length = word.length;
                                     const classes = classnames(
                                         "word", {
@@ -41,7 +51,7 @@ class WordsGenerator extends React.PureComponent {
                                     );
 
                                     return (
-                                        <div className={classes}>
+                                        <div key={index} className={classes}>
                                             <div>{word}</div>
                                         </div>
                                     );
